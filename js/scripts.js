3981825a4084cb658cc316796dbcfff38657f808
JoHannah Joy and Lee Pedersen <johannah@>
2019-10-22 11:23:43 -0700

    add jQuery methods with buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
     $("#ghost-showing").toggle();
     $("#ghost-hidden").toggle();
   });
+  $(".quote-click").click(function() {
+    $("#ghost-quotes").slideDown().delay( 1400 ).slideUp();
+  });
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+  });
+  $("button#orange").click(function() {
+    $("body").removeClass();
+    $("body").addClass("orange-background");
+  });
+  $("button#default").click(function() {
+    $("body").removeClass();
+    $("body").addClass("default-background");
+  });
 });
